fde7ed7b84edc4af107b3da39f19e72dade01454
Breno Lemos <brenomiros@gmail.com>
2021-10-18 21:52:24 -0300

    Layout component

--- src/components/template/Layout.tsx
+++ src/components/template/Layout.tsx
@@ -10,10 +10,12 @@ interface LayoutProps {
 
 export default function Layout(props: LayoutProps) {
   return (
-    <div>
-      <Header title={props.title} subtitle={props.subtitle} />
+    <div className={`flex h-screen w-screen `}>
       <SideMenu />
-      <Content>{props.children}</Content>
+      <div className={`flex flex-col w-full p-7 bg-gray-300`}>
+        <Header title={props.title} subtitle={props.subtitle} />
+        <Content>{props.children}</Content>
+      </div>
     </div>
   );
 }
