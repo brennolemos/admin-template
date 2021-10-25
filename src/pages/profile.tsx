a6bb8868d4a4771889c9e2c83bcf663f1db7821f
Breno Lemos <brenomiros@gmail.com>
2021-10-25 17:26:52 -0300

    User Avatar

---
+++ src/pages/profile.tsx
@@ -0,0 +1,11 @@
+import Layout from "../components/template/Layout";
+
+export default function Profile() {
+  return (
+    <div>
+      <Layout title="User Profile" subtitle="User Informations">
+        <h1>User Profile</h1>
+      </Layout>
+    </div>
+  );
+}
